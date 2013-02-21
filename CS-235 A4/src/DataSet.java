048eed9a28a3d63e4f653ed5323dc697bd3d27c1
Gwion <Gwion@137.44.96.100>
2013-02-21 16:37:41 +0000

    Added some doxygen stuff :P

--- CS-235 A4/src/DataSet.java
+++ CS-235 A4/src/DataSet.java
@@ -1,30 +1,66 @@
+/**
+ * \This class checks the received file to see if the file valid and then
+ * stores the attribute data in one single dimensional array with the data
+ * values in a 2 dimensional array.
+ * 
+ * @author 	-Gwion Rhys Davies
+ * @file 	-DataSet.java
+ * @data	-15 Feb '13
+ */
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-
 public class DataSet {
 
+	/** */
 	private Scanner m_fileScan;
+	/** */
 	private String m_delimiter;
+	/** */
 	private Scanner m_lineScanner;
+	/** */
 	private int m_attributeCount;
+	/** */
 	private int m_entryCount;
+	/** */
 	private int m_lineDataCount;
+	/** */
 	private String[] m_attributes;
-	private double[][] m_data;
+	/** */
+	private Object[][] m_data;
+	/** */
 	private String m_dataEntry;
+	/** */
 	private int m_emptyEntryCount;
-	private double[] m_columnData;
-
-	public double[] getColumnData(int m_columnNo){
-		if(!(m_columnNo<=m_attributeCount)){
+	/** */
+	private Object[] m_columnData;
+	
+	/**
+	    * @return the 2 dimensional array of data values.
+	    */
+	public Object [][] GetData() {
+		return m_data;
+	}
+	/**
+	    * @return the array of attribute values.
+	    */
+	public String[] GetAttributes() {
+		return m_attributes;
+	}
+	/**
+	    * @return an array containing a column data values, the column of 
+	    * values returned is chosen by the given argument integer which must
+	    * be a value between 0 and the number of attributes in the data set.
+	    */
+	public Object[] getColumnData(int m_columnNo){
+		if(!(m_columnNo<m_attributeCount)){
 			System.out.println("data entry error");
 		} else {
-			m_columnData = new double[m_entryCount];
+			m_columnData = new Object[m_entryCount];
 			for(int i = 0; i<m_entryCount; i++){
 				m_columnData[i] = m_data[m_columnNo][i];
 			}
@@ -32,24 +68,35 @@ public class DataSet {
 		}
 		return null;
 	}
-
+	/**
+	    * @return a String of the 'nth' attribute of the attribute array 'nth'
+	    * term being the given argument integer which must be a value between
+	    * 0 and the number of attributes in the data set.
+	    */
 	public String getAttributeName(int m_columnNo){
-		if(!(m_columnNo<=m_attributeCount)){
+		if(!(m_columnNo<m_attributeCount)){
 			System.out.println("column entry error");
 		} else {
 			return m_attributes[m_columnNo];
 		}
 		return null;
 	}
-
+	/**
+	    * @return the number of entries in the data set.
+	    */
 	public int getNoOfEntrys(){
 		return m_entryCount;
 	}
-
+	/**
+	    * @return the number of attributes in the data set.
+	    */
 	public int getNoOfAttributes(){
 		return m_attributeCount;
 	}
-	
+	/**
+	    * @return TRUE on success if the csv file is valid and the data set
+	    * has been successfully built.
+	    */
 	public Boolean buildDataSet (File m_dataFile) {
 		if(!checkForValidFile(m_dataFile)){
 			return false;
@@ -66,7 +113,7 @@ public class DataSet {
 		for(int i=0; i<m_attributeCount; i++){
 			m_attributes[i]=m_lineScanner.next();
 		}
-		m_data = new double[m_attributeCount][m_entryCount];
+		m_data = new Object[m_attributeCount][m_entryCount];
 		for(int i=0; i<m_entryCount; i++){
 			m_lineScanner = new Scanner(m_fileScan.nextLine());
 			m_lineScanner.useDelimiter(m_delimiter);
@@ -86,7 +133,9 @@ public class DataSet {
 		System.out.println("Data Set Built");
 		return true;
 	}
-	
+	/**
+	    * @return TRUE on success if all validation tests return true.
+	    */
 	private Boolean checkForValidFile(File m_dataFile){
 		final JPanel m_frame = new JPanel();
 		if(!checkForCommas(m_dataFile)){
@@ -139,8 +188,7 @@ public class DataSet {
 						return false; 
 					}
 				}
-				m_lineDataCount++;
-				
+				m_lineDataCount++;	
 			}
 			if(!(m_lineDataCount-m_emptyEntryCount==m_attributeCount)){
 				if (!(m_lineDataCount-m_emptyEntryCount==0)){
@@ -153,7 +201,9 @@ public class DataSet {
 		}
 		return true;
 	}
-
+	/**
+	    * @return TRUE on success.
+	    */
 	private int countAttributes(File m_dataFile) {
 		int m_numberOfAttributes = 0;
 		m_delimiter=",";
@@ -172,7 +222,9 @@ public class DataSet {
 		System.out.println(m_numberOfAttributes);
 		return m_numberOfAttributes;
 	}
-
+	/**
+	    * @return TRUE on success.
+	    */
 	private boolean checkForAttributes(File m_dataFile) {
 		try {
 			m_fileScan = new Scanner(m_dataFile);
@@ -192,7 +244,9 @@ public class DataSet {
 		}
 		return false; 
 	}
-	
+	/**
+	    * @return TRUE on success.
+	    */
 	private Boolean checkForCommas (File m_dataFile) {
 		m_delimiter="";
 		try {
@@ -210,4 +264,4 @@ public class DataSet {
 		}
 		return false;
 	}
-}
+} /** end class DataSet */
